424190f1954c94b497a69630d672b7afc88119ca
hthomp15 <hailethomp@gmail.com>
2022-03-19 17:47:39 -0400

    create database tables

--- db/schema.sql
+++ db/schema.sql
@@ -3,21 +3,24 @@ DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS role;
 
 CREATE TABLE employee (
-    id: INTEGER AUTO_INCREMENT PRIMARY KEY,
-	first_name: VARCHAR(30) NOT NULL,
-	last_name: VARCHAR(30) NOT NULL,
-	role_id: VARCHAR(30) INT NOT NULL,
-	manager_id: INT
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	role_id VARCHAR(30) INT NOT NULL,
+	manager_id INT
+	CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+	CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
 
 CREATE TABLE department (
-    id: INTEGER PRIMARY KEY,
-	name: VARCHAR(30) NOT NULL /* (department name) */
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(30) NOT NULL /* (department name) */
 )
 
 CREATE TABLE role (
-    id: INTEGER AUTO_INCREMENT PRIMARY KEY,
-	title: VARCHAR(30) NOT NULL,
-	salary: DECIMAL,
-	department_id: INTEGER, 
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	title VARCHAR(30) NOT NULL,
+	salary DECIMAL NOT NULL,
+	department_id INTEGER NOT NULL, 
+	CONSTRAINT fk_department FOREIGN KEY (department_id) REFRENCES department(id) ON DELETE CASCADE
 )
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,33 @@
 Insert INTO employees (id, first_name, last_name, role_id, manager_name)    
     VALUES 
-        (''),
-        (''),
+        ('Haile', 'Thompson', 7, NULL),
+        ('Lacey', 'Wisdom', 5, NULL),
+        ('Austin', 'Jocobs', 1, 2),
+        ('Whitney', 'Huston', 4, NULL),
+        ('Koco', 'Withorn', 9, 1),
+        ('Simba', 'Lawrence', 8, 1),
+        ('Connnie' 'Kimm' 2, 2),
+        ('Travon', 'Diggins', 6, 4)
         
+
+INSERT INTO role(title, salary, department_id)
+VALUES
+('Salesperson', 80000, 1),
+('Business Manager', 110000, 1),
+('Marketing Manager', 80000, 2),
+('Branding Executive', 140000, 2),
+('Account Manager', 150000, 3),
+('Accountant', 100000, 3),
+('Senior Software Engineer', 200000, 4),
+('Software Engineer', 120000, 4),
+('Administrative Assistant', 60000, 5),
+('Office Manager', 100000, 5);
+
+   
+INSERT INTO department(name)
+VALUES
+('Sales'),
+('Marketing'),
+('Accounting'),
+('Engineering'),
+('Administration');
